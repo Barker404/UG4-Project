3472bf5eb7e59a1681b3bfbedd609fe1e8e5d9a5
Lewis Barker <l.barker.404@gmail.com>
2015-12-03 04:19:51 +0000

    Add show algorithm which only shows each message to a single (best) node

--- showing.py
+++ showing.py
@@ -59,3 +59,35 @@ class FurtherProbShowModel(ShowModel):
     def select_final(self, candidates):
         # Select the correct amount randomly
         return random.sample(candidates, min(self.max_shown, len(candidates)))
+
+
+class OnlyBestShowModel(ShowModel):
+
+    def __init__(self, max_shown=20):
+        self.max_shown = max_shown
+
+    def show_alg(self, g, node_index, possible):
+        # The first candidates
+        return self.select_candidates(g, node_index, possible)[:self.max_shown]
+
+    def select_candidates(self, g, node_index, possible):
+        # Return only those messages for which this node is best
+        return [p for p in possible if (
+            self.best_node(g, p[0], p[1]) == node_index)]
+
+    def best_node(self, g, message, current_node_index):
+        min_index = None
+        min_distance = float('inf')
+
+        for neighbour_index in g.neighbors_iter(current_node_index):
+            distance = grid_distance(neighbour_index, message.destination)
+
+            # Ensure that we not only get the min, but always the same index
+            # (in the case that distances are the same
+            # - even if node iteration order changes)
+            if distance < min_distance or (
+                    distance == min_distance and neighbour_index < min_index):
+                min_distance = distance
+                min_index = neighbour_index
+
+        return min_index
